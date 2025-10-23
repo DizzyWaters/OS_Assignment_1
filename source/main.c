3656bc1f69e3d4ae05060fd03e9040acc2bdbd3d
DizzyServer <mkulvanovskis@gmail.com>
2025-10-23 11:00:17 -0400

    first block sysinfo added

--- source/main.c
+++ source/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <sys/utsname.h>
+#include <unistd.h>
+
 
 int main(int argc, char **argv)
 {
-    printf("Hello World");
-    
+    struct utsname sysinfo;
+    if(uname(&sysinfo) == -1)
+    {
+        perror("uname error!");
+    }
+    printf("Hello this is the System information program:\n");
+    printf("System name:    %s\n", sysinfo.sysname);
+    printf("Node name:  %s\n", sysinfo.nodename);
+    printf("Release:    %s\n", sysinfo.release);
+    printf("Version:    %s\n", sysinfo.version);
+    printf("Machine:    %s\n", sysinfo.machine);
     return 0;
 }
