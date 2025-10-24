12b923f9621aac4d07fb8882ed9d1908da1d451d
DizzyServer <mkulvanovskis@gmail.com>
2025-10-24 05:05:07 -0400

    File system(mounts) and some documentation added

--- source/main.c
+++ source/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 // for sysinfo block
 #include <sys/utsname.h>
 // for gatting host block
@@ -7,7 +8,13 @@
 #include <time.h>
 // stat blocl - drives
 #include <sys/statvfs.h>
+#include <mntent.h> // this one is basicly loop macro to go threw dir of mounted drives
 
+
+// defines
+
+#define D_LONG_WORD 256
+#define D_SHORT_WORD 64
 
 int main(int argc, char **argv)
 {
@@ -37,6 +44,42 @@ int main(int argc, char **argv)
     printf("SYSTEM Time is:    %s\n", asctime(ptr));
     printf("LOCAL Time is:    %s\n", asctime(ptr2));
     // System Drive Block
+
+    FILE *myDrives = fopen("/etc/mtab", "r");
+    if(!myDrives)
+    {
+        perror("fopen failed");
+        printf("stoped here -> path is crap");
+        return 1;
+    }
+
+    char device[D_LONG_WORD];
+    char mountpoint[D_LONG_WORD];
+    char fstype[D_SHORT_WORD];
+    char options[D_SHORT_WORD];
+    int dump;
+    int pass;
+
+
+    struct mntent *mnt;
+
+    printf("%-30s %-30s %-30s %-30s %-30s\n", "Device", "Mount Point", "Type", "Total MegaB", "Free MegaB"); // names
+    printf("***************************************************************************************************************\n");
+        while((mnt = getmntent(myDrives)) != NULL)
+        {
+            struct statvfs vfs;
+                if(statvfs(mnt->mnt_dir, &vfs) == 0){
+                    unsigned long totalSpace = (vfs.f_blocks * vfs.f_frsize ) / (1024 * 1024);
+                    unsigned long freeSpace = (vfs.f_bfree * vfs.f_frsize ) / (1024 * 1024);
+                    printf("%-30s %-30s %-30s %-30lu %30lu\n", mnt->mnt_fsname, mnt->mnt_dir, mnt->mnt_type, totalSpace, freeSpace);
+                }
+        }
+        endmntent(myDrives);
+
+
+
+
+
 
 
 
